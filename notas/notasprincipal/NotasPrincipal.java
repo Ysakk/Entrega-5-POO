79c36dcc1f2ff185fb183908da7661b21573f46f
Ysakk <82222856+Ysakk@users.noreply.github.com>
2022-11-19 17:14:41 -0500

    Add files via upload

---
+++ notas/notasprincipal/NotasPrincipal.java
@@ -0,0 +1,16 @@
+package notas.notasprincipal;
+
+/**
+ *
+ * @author Isaac
+ */
+public class NotasPrincipal {
+/**
+* Método main que sirve de punto de entrada al programa
+*/
+public static void main(String[] args) {
+    VentanaPrincipal miVentanaPrincipal; /* Define la ventana principal */
+    miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana principal */
+    miVentanaPrincipal.setVisible(true); /* Establece la ventana como visible */
+}
+}
